e7af2ae04a2631601597539221f87fe20ed8bd6b
pixelhijack <pothattila@gmail.com>
2017-04-10 21:27:26 +0200

    [PHASER] Player, enemy sprites. Manual movement updates

--- client/src/gamestates/play.js
+++ client/src/gamestates/play.js
@@ -1,6 +1,9 @@
 class GameState{
     constructor(gameConfig) {
         this.gameConfig = gameConfig;
+        this.keys = undefined;
+        this.player = undefined;
+        this.enemy = undefined;
     }
 
     init(configs){
@@ -9,12 +12,47 @@ class GameState{
 
     preload(){
         console.log('[PHASER][Component][Preload]');
+        this.game.load.image('player', 'man.png');
+        this.game.load.image('dino', 'dino.png');
     }
     create(){
         console.log('[PHASER][Component][Create]');
+        // fps debugging
+        this.game.time.advancedTiming = true;
+
+        // [SET LEVEL] set dimensions, start physic system
+        this.game.world.setBounds(
+            0,
+            0,
+            this.gameConfig.width,
+            this.gameConfig.height
+        );
+
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        // [PLAYER]
+        this.player = this.game.add.sprite(200, 200, 'player');
+
+        // [ENEMY]
+        this.enemy = this.game.add.sprite(400, 200, 'dino');
+
+        this.game.camera.follow(this.player);
+
+        // bind keys
+        this.keys = this.game.input.keyboard.createCursorKeys();
+        this.keys.space = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     }
     update(){
         console.log('[PHASER][Component][Update]');
+        // fps
+        this.game.debug.text(this.game.time.fps, 5, 20);
+
+        // move
+        if(this.keys.left.isDown){
+            this.player.x--;
+        } else if(this.keys.right.isDown){
+            this.player.x++;
+        }
     }
 }
 
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const server = http.createServer(app);
 
 app.use(express.static(path.resolve(__dirname, 'client')));
+app.use(express.static(path.resolve(__dirname, 'assets')));
 
 server.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0", function(){
   const addr = server.address();
