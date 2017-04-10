54e956aae42388244d19b40bbb4f89266cbfee3b
pixelhijack <pothattila@gmail.com>
2017-04-10 19:55:58 +0200

    [BUILD] Webpack build process setup. Game bootstrapped, check the console!

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'client/dist');
+const APP_DIR = path.resolve(__dirname, 'client/src');
+
+const config = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
+
+module.exports = config;
