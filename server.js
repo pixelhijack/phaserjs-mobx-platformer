d81eb45d358d373109aa74101fceaa3c7ab6fd6d
pixelhijack <pothattila@gmail.com>
2017-04-10 19:42:29 +0200

    [SERVER] .gitignore, npm start script, express.js added

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static(path.resolve(__dirname, 'client')));
+
+server.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0", function(){
+  const addr = server.address();
+  console.log("Server listening at", addr.address + ":" + addr.port);
+});
