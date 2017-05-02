d266e605119575675fd3d7fa0a9b0040d2f0c010
pixelhijack <pothattila@gmail.com>
2017-05-02 09:43:02 +0200

    [SCALE] Scaling up the canvas. Beware of the FPS drop!

--- client/src/gamestates/play/play.preload.js
+++ client/src/gamestates/play/play.preload.js
@@ -1,6 +1,13 @@
 function preload(){
     console.log('[PHASER][Component][Preload]');
 
+    // ------! FPS killer: performance drop on scaling up more than 1.6x
+    this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.game.scale.pageAlignHorizontally = true;
+    this.game.scale.pageAlignVertically = true;
+    this.scale.setMinMax(0, 0, this.globalConfig.width * 1.6, this.globalConfig.height * 1.6);
+    // ------!
+
     // assets to load relative to /assets/..
     this.game.load.atlas(
         'pre2atlas',
